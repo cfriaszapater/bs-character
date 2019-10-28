0475fea79f70e84df1a9095e0bfa5b763fa2f072
c.friaszapater <c.friaszapater@orwellg.com>
2019-10-28 17:29:28 +0100

    display min fraction if value approaches minimum

--- src/components/character/fractionForCharacteristic.spec.ts
+++ src/components/character/fractionForCharacteristic.spec.ts
@@ -1,5 +1,9 @@
 import { fractionForCharacteristic } from "./fractionForCharacteristic";
 
-it("should return relative percentage of characteristic", () => {
+it("should return relative fraction", () => {
   expect(fractionForCharacteristic(15, 0, 20)).toBe(0.75);
+});
+
+it("should return minimum fraction", () => {
+  expect(fractionForCharacteristic(2, 3, 6)).toBe(0.05);
 });
--- src/components/character/fractionForCharacteristic.tsx
+++ src/components/character/fractionForCharacteristic.tsx
@@ -1,7 +1,9 @@
+const minFraction = 0.05;
+
 export function fractionForCharacteristic(
   value: number,
   min: number,
   max: number
 ) {
-  return Math.max(value - min, 0) / (max - min);
+  return Math.max((value - min) / (max - min), minFraction);
 }
